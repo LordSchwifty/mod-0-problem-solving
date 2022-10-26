6fb3fc0dcde36f12d655aa076d1e06d64ba2693a
Tom Doder <tdoder89@gmail.com>
2022-10-26 14:40:01 -0500

    show 3 challenges

---
+++ problem_solving.js
@@ -0,0 +1,10 @@
+var animals = ["cat", "treefrog", "tabby cat", "fish"] 
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].length > 3) {
+        console.log(animals[i];)
+    }
+}
+var animals =["snake", "sfish", "cat"]
+for (var i =0; i < animals.length; i++) {
+    if (animals[i].startsWith = (s))
+}
